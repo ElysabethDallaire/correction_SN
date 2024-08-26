683253e5ef08cf444e3217fdbab08568399204be
ElysabethDallaire <elysabethdallaire@cegepjonquiere.ca>
2024-08-26 18:46:44 -0400

    Add files via upload

---
+++ semaine_01/p.39-40/opt3_volume.py
@@ -0,0 +1,10 @@
+"""
+Elysabeth Dallaire
+Optionnel #3 Volume
+"""
+
+#declaration des variables
+r = 3
+pi = 3.1416
+volume = (4 * pi * (r**3))/3
+print("Le volume de la sphère est " + str(volume) + "m^3")
