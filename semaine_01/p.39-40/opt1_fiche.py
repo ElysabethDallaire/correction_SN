ffa8a803d96be0be63aa77ca32fc9b38f6e1cd1d
ElysabethDallaire <elysabethdallaire@cegepjonquiere.ca>
2024-09-05 16:34:22 -0400

    Add files via upload

---
+++ semaine_01/p.39-40/opt1_fiche.py
@@ -0,0 +1,13 @@
+"""
+Elysabeth Dallaire
+Optionnel #1 Fiche
+"""
+#Declaration des variables
+nom = input("Votre nom : ")
+age = int(input("Votre age : "))
+majeur = True
+
+#Affichage
+print("Je m'appelle " + nom)
+print("J'ai " + str(age) + " ans")
+print("Je suis un adulte : " + str(majeur))
