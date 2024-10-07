2b24d0271d896cd2cf4607b26ee94c48a7ed02ab
ElysabethDallaire <elysabethdallaire@cegepjonquiere.ca>
2024-10-07 18:05:11 -0400

    Add files via upload

---
+++ semaine_06/p.59/opt10_moyenne.py
@@ -0,0 +1,13 @@
+"""
+Elysabeth Dallaire
+Optionnel #10 - Moyenne
+"""
+
+#Définition de la liste
+serie = [2, 7, 4, 20, 18, 13, 15, 16, 10, 11]
+
+#Calcul de la moyenne
+moyenne = sum(serie) / len(serie)
+
+#Affichage
+print(f"La moyenne de la série est : {moyenne}")
---
+++ semaine_06/p.59/opt11_trigo.py
@@ -0,0 +1,12 @@
+"""
+Elysabeth Dallaire
+Optionnel #11 - Trigo
+"""
+
+import math
+
+#Calcul de la valeur
+valeur = 3 * math.pi * (math.sin(3) + math.tan(1) - math.log(3))
+
+#Affichage
+print(f"Selon la formule, la valeur finale est : {valeur} (Valeur arrondie : {round(valeur, 2)})")
